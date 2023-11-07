610fb0deb63e846ae5ba5632dd27cde0378aec5f
Thomas66 <thomassalvaterra2001@gmail.com>
2023-11-07 17:05:35 +0100

    second version

--- main.js
+++ main.js
@@ -3,31 +3,31 @@ let bottone = document.getElementById("btnPlay");
 
 btnPlay.addEventListener("click", function() {
     console.log("Bottone cliccato");
-});
-
 
 //creare la griglia dinamicamente, inserendo N elementi .square nel container
-let numCelle = 100;
-let grid = document.getElementById("grid");
 
-for (let i = 1; i <= numCelle; i++) {
+    let numCelle = 100;
+    let grid = document.getElementById("grid");
 
-    let cella = creaQuadrato(i);
-    grid.appendChild(cella);
-    document.getElementsById("cella").innerHTML = creaQuadrato(i);
-}
+    for (let i = 1; i <= numCelle; i++) {
 
-function creaQuadrato(numero) {
+        let cella = creaQuadrato(i);
+        grid.appendChild(cella);
+        cella.innerHTML = (i); 
+    }   
 
-    const cella = document.createElement("div");
-    cella.classList.add("square");
+    function creaQuadrato(numero) {
 
-    //per ogni quadrato voglio un evento che gestisca il click
-    cella.addEventListener("click", function() {
-        console.log("Cella cliccata", numero);
-        cella.classList.toggle("evidenziata");
+        const cella = document.createElement("div");
+        cella.classList.add("square");
 
-    });
+        //per ogni quadrato voglio un evento che gestisca il click
+        cella.addEventListener("click", function() {
+            console.log("Cella cliccata", numero);
+            cella.classList.toggle("evidenziata");
 
-    return cella;
-}
+        });
+
+        return cella;
+    }   
+});
